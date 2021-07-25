cefbd3b3a4dd0de8c57484f4dd81fe5105b2b8e5
Nhin <nhinlechi@gmail.com>
2021-07-25 10:13:24 +0700

    Fix bug tooltip displayed on top of menus item

--- src/components/widgets/Dropdown.js
+++ src/components/widgets/Dropdown.js
@@ -1,71 +1,74 @@
-import { Button, MenuList } from "@material-ui/core";
+import { Button, MenuList, Toolbar } from "@material-ui/core";
 import MenuItem from "@material-ui/core/MenuItem";
 import { useState, useRef, useEffect } from "react";
 import AppTheme from "../../constants/theme";
 import Grow from "@material-ui/core/Grow";
 import Paper from "@material-ui/core/Paper";
 import Popper from "@material-ui/core/Popper";
+import { Tooltip } from "@material-ui/core";
 
 export function CustomDropDown(props) {
-    const [open, setOpen] = useState(false);
-    const anchorRef = useRef(null);
-  
-    const handleToggle = () => {
-      setOpen((prevOpen) => !prevOpen);
-    };
-  
-    const handleClose = (event) => {
-      if (anchorRef.current && anchorRef.current.contains(event.target)) {
-        return;
-      }
-      setOpen(false);
-    };
-  
-    const clickItemHandler = (event, index) => {
-      props.clickItemCallback(index);
-      handleClose(event);
-    };
-  
-    // return focus to the button when we transitioned from !open -> open
-    const prevOpen = useRef(open);
-    useEffect(() => {
-      if (prevOpen.current === true && open === false) {
-        anchorRef.current.focus();
-      }
-  
-      prevOpen.current = open;
-    }, [open]);
-  
-    return (
-        <div>
-          <Button
-            ref={anchorRef}
-            aria-controls="customized-menu"
-            aria-haspopup="true"
-            variant="contained"
-            style={{ backgroundColor: AppTheme.primary }}
-            onClick={handleToggle}
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const clickItemHandler = (event, index) => {
+    props.clickItemCallback(index);
+    handleClose(event);
+  };
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div>
+      <Button
+        ref={anchorRef}
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        style={{ backgroundColor: AppTheme.primary }}
+        onClick={handleToggle}
+      >
+        <text style={{ color: AppTheme.secondary }}>
+          {props.options[props.selectedIndex ?? 0]}
+        </text>
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
           >
-            <text style={{ color: AppTheme.secondary }}>
-              {props.options[props.selectedIndex ?? 0]}
-            </text>
-          </Button>
-          <Popper
-            open={open}
-            anchorEl={anchorRef.current}
-            role={undefined}
-            transition
-            disablePortal
-          >
-            {({ TransitionProps, placement }) => (
-              <Grow
-                {...TransitionProps}
-                style={{
-                  transformOrigin:
-                    placement === "bottom" ? "center top" : "center bottom",
-                }}
-              >
-                <Paper>
+            <Paper>
+              <Toolbar>
+                <Tooltip>
                   <MenuList autoFocusItem={open} id="menu-list-grow">
                     {props.options.map((value, index) => {
                       return (
@@ -78,11 +81,12 @@ export function CustomDropDown(props) {
                       );
                     })}
                   </MenuList>
-                </Paper>
-              </Grow>
-            )}
-          </Popper>
-        </div>
-    );
-  }
-  
+                </Tooltip>
+              </Toolbar>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
+  );
+}
